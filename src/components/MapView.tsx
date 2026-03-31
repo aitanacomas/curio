4966a71751377f562b2e633cd355fb859d5ff59e
Aitana Comas <aitanacomas@Aitanas-MacBook-Pro.local>
2026-03-30 20:45:05 -0700

    Push label minZoom to 8 so names only appear at city-level zoom

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -74,7 +74,7 @@ export default function MapView({ places, center = [20, 10], zoom = 2, height = 
       maxBoundsViscosity={1.0}
     >
       <TileLayer url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png" />
-      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png" minZoom={6} />
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png" minZoom={8} />
       {places.length === 0 && <FitWorld />}
       {places.length > 1 && <FitBounds places={places} />}
       {places.map(place => (
